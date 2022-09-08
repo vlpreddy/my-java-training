360c2d57a92102fc1d56fe913963c3d7f499ecde
leela Parameswara reddy <info@redmindstech.com>
2022-09-09 01:02:44 +0530

    updated methods in java

--- expressionsstatements/src/Main.java
+++ expressionsstatements/src/Main.java
@@ -4,6 +4,24 @@ public class Main {
         calculateScore(true, 10000, 8,200);
         int highscore = calculateScoreReturn(true, 800, 5, 100);
         System.out.println("final Score " + highscore);
+
+        int highScoreposition = calculateHighestScorePosition(1500);
+        displayHighestScorePosition("Tim", highScoreposition);
+
+        highScoreposition = calculateHighestScorePosition(900);
+        displayHighestScorePosition("Bob", highScoreposition);
+
+        highScoreposition = calculateHighestScorePosition(400);
+        displayHighestScorePosition("Percy", highScoreposition);
+
+        highScoreposition = calculateHighestScorePosition(50);
+        displayHighestScorePosition("Gilbert", highScoreposition);
+
+        highScoreposition = calculateHighestScorePosition(1000);
+        displayHighestScorePosition("Leela", highScoreposition);
+
+        highScoreposition = calculateHighestScorePosition(500);
+        displayHighestScorePosition("Reddy", highScoreposition);
     }
 
     public static void calculateScore(boolean gameover, int score, int levelcompleted, int bonus){
@@ -22,6 +40,34 @@ public class Main {
             return finalScore;
         }
         return -1;
+    }
+
+    public static void displayHighestScorePosition(String playerName, int highScorePosition){
+        System.out.println(playerName + " managed to get into position " + highScorePosition + " on the highest score table ");
+    }
+
+    public static int calculateHighestScorePosition(int playerScore){
+       /*
+        if(playerScore >= 1000) {
+            return 1;
+        } else if(playerScore >= 500) {
+            return 2;
+        }else if(playerScore >= 100) {
+             return 3;
+        }
+        return 4;
+     */
+        int position = 4;
+        if(playerScore >=1000) {
+            position=1;
+        } else if(playerScore >=500) {
+            position=2;
+        } else if(playerScore >=100) {
+            position=3;
+        }
+
+        return position;
+
     }
 
 
